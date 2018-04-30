ef89e4b255d1042ea019fd08b422be424f9db7e7
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-04-30 11:14:20 -0500

    Tic-Tac_Toe Game
    Tic-Tac-Toe in a dictionary

---
+++ ticTacToe.py
@@ -0,0 +1,9 @@
+#Python program for tic tac toe develeoped for my kids
+#Game is stored as dictonary values representing the hash shaped board
+#The slots are represented as tL - for top left, mC for middle center, bR - for bottom right ect. 
+#The key-value pairs will consist of the square designator and either 'X', 'O', or '' 
+
+Board = {'tL': '', 'tC': '', 'tR': '',
+		 'mL': '', 'mC': '', 'mR': '',
+		 'bL': '', 'bC': '', 'bR': ''}
+		 
